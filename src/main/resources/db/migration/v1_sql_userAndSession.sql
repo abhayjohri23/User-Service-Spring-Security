f1366a3358051c5b32e1c57228c38a2abca3eae5
abhayjohri23 <abhay.johri0@gmail.com>
2024-02-09 16:51:11 +0530

    Versioned Initial

---
+++ src/main/resources/db/migration/v1_sql_userAndSession.sql
@@ -0,0 +1,20 @@
+CREATE TABLE sessions
+(
+    entity_id        BIGINT NOT NULL,
+    user_id          BIGINT NULL,
+    date_of_issuance date NULL,
+    time_of_issuance time NULL,
+    session_token    BINARY(16) NULL,
+    CONSTRAINT pk_sessions PRIMARY KEY (entity_id)
+);
+
+CREATE TABLE users
+(
+    entity_id       BIGINT NOT NULL,
+    username        VARCHAR(255) NULL,
+    bcrypt_password VARCHAR(255) NULL,
+    CONSTRAINT pk_users PRIMARY KEY (entity_id)
+);
+
+ALTER TABLE sessions
+    ADD CONSTRAINT FK_SESSIONS_ON_USERID FOREIGN KEY (user_id) REFERENCES users (entity_id);
